1e3f6fe4de8cd83f68d427094491efa450afdef9
Jonathan Picques <jonathan.picques@gmail.com>
2016-05-23 21:51:41 +0200

    Removed ES5 build

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,10 @@ const notify = require("gulp-notify");
 const rename = require("gulp-rename");
 const plumber = require("gulp-plumber");
 const sourcemaps = require("gulp-sourcemaps");
-const babel = require("rollup-plugin-babel");
-const nodeResolve = require("rollup-plugin-node-resolve");
-const uglify = require("rollup-plugin-uglify");
 
 const sources = ["src/*.js", "src/**/*.js"];
-const es5config = {
-    "format": "umd",
-    "sourceMap": true,
-    "moduleName": "dudeGraph",
-    "plugins": [
-        babel({
-            "presets": ["es2015-rollup"],
-            "babelrc": false,
-            "sourceMap": true
-        }),
-        nodeResolve(),
-        uglify()
-    ]
-};
-const es6config = {
-    "sourceMap": true
-};
 
-gulp.task("default", ["build:es5", "build:es6"]);
+gulp.task("default", ["build"]);
 
 gulp.task("watch", ["default"], () => {
     gulp.watch(sources, ["default"]);
@@ -45,18 +25,11 @@ gulp.task("lint", () => {
         }));
 });
 
-gulp.task("build:es5", ["lint"], () => {
+gulp.task("build", ["lint"], () => {
     return gulp.src("src/dude-graph.js")
-        .pipe(rollup(es5config))
-        .pipe(rename("dude-graph.js"))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("dist/"));
-});
-
-gulp.task("build:es6", ["lint"], () => {
-    return gulp.src("src/dude-graph.js")
-        .pipe(rollup(es6config))
-        .pipe(rename("dude-graph-es6.js"))
+        .pipe(rollup({
+            "sourceMap": true
+        }))
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("dist/"));
 });
