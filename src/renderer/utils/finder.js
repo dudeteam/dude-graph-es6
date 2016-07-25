e81d20e233c9aa6217adcc9bfc78b6aa4f931ab4
Jonathan Picques <jonathan.picques@gmail.com>
2016-07-26 01:52:41 +0200

    Added nearest render blocks

--- src/renderer/utils/finder.js
+++ src/renderer/utils/finder.js
@@ -11,21 +11,33 @@ export default class RenderNodeFinder {
     }
 
     /**
+     * Returns the nearest render point of the specified area
+     * @param {Array<number>} area - specifies the area
+     * @returns {Array<RenderBlock>}
+     */
+    nearestRenderBlocks(area) {
+        return this[_renderer].renderBlocks.filter((renderBlock) => {
+            if (renderBlock.position[0] + renderBlock.size[0] < area[0]) { return false; }
+            if (renderBlock.position[0] > area[0] + area[2]) { return false; }
+            if (renderBlock.position[1] + renderBlock.size[1] < area[1]) { return false; }
+            if (renderBlock.position[1] > area[1] + area[3]) { return false; }
+            return true;
+        });
+    }
+
+    /**
      * Returns the nearest render point of the specified position
      * @param {Array<number>} position - specifies the position
      * @returns {RenderPoint|null}
      */
     nearestRenderPoint(position) {
-        const renderPoints = this[_renderer].renderPoints;
-        for (const renderPoint of renderPoints)  {
-            if (renderPoint.absolutePosition[0] > position[0] - this[_renderer].config.point.height &&
-                renderPoint.absolutePosition[0] < position[0] + this[_renderer].config.point.height &&
-                renderPoint.absolutePosition[1] > position[1] - this[_renderer].config.point.height &&
-                renderPoint.absolutePosition[1] < position[1] + this[_renderer].config.point.height) {
-                return renderPoint;
-            }
-        }
-        return null;
+        return this[_renderer].renderPoints.find((renderPoint) => {
+            if (renderPoint.absolutePosition[0] + this[_renderer].config.point.radius < position[0]) { return false; }
+            if (renderPoint.absolutePosition[0] > position[0] + this[_renderer].config.point.radius) { return false; }
+            if (renderPoint.absolutePosition[1] + this[_renderer].config.point.radius < position[1]) { return false; }
+            if (renderPoint.absolutePosition[1] > position[1] + this[_renderer].config.point.radius) { return false; }
+            return true;
+        }) || null;
     }
 
 }
