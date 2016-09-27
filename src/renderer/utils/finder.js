b0e4a17fad692b084261a224995c4e12155aef3d
Vincent <20cent.neel@gmail.com>
2016-09-27 17:27:41 +0200

    Added nearestRenderPoint function to finder.js.

--- src/renderer/utils/finder.js
+++ src/renderer/utils/finder.js
@@ -26,6 +26,26 @@ export default class RenderNodeFinder {
     }
 
     /**
+     * Returns the nearest render groups of the specified area
+     * @param {RenderBlock} renderBlock - the render group
+     * @returns {RenderGroup}
+     */
+    nearestRenderGroup(renderBlock) {
+        const groups = this[_renderer].renderGroups.filter((renderGroup) => {
+            if (renderGroup.position[0] > renderBlock.position[0]) { return false; }
+            if (renderGroup.position[1] > renderBlock.position[1]) { return false; }
+            if (renderGroup.position[0] + renderGroup.size[0] < renderBlock.position[0]) { return false; }
+            if (renderGroup.position[1] + renderGroup.size[1] < renderBlock.position[1]) { return false; }
+            return true;
+        }).sort((rg1, rg2) => {
+            const index1 = Array.prototype.indexOf.call(this[_renderer].svgGroups.element.childNodes, rg1.element.element);
+            const index2 = Array.prototype.indexOf.call(this[_renderer].svgGroups.element.childNodes, rg2.element.element);
+            return index1 < index2 ? 1 : -1;
+        });
+        return groups.length > 0 ? groups[0] : null;
+    }
+
+    /**
      * Returns the nearest render point of the specified position
      * @param {Array<number>} position - specifies the position
      * @returns {RenderPoint|null}
